cd70fd35980c44317f687fc50bedc3a7f2766fa5
dnl1vieira <daniielvieira@outlook.com>
2020-11-08 16:48:28 -0300

    arrow: Adjust array of events

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   arrow;
   isClose = true;
-  events: string[] = ["open!"];
+  events: string[] = [];
   opened: boolean;
 
   shouldRun = true;
